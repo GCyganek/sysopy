d97bda03e78d1bd7baec3dd395099a719c3d7531
gcyganek <gabrielcyganek@gmail.com>
2021-03-22 19:28:33 +0100

    cw03/zad1 completed

---
+++ cw03/zad1/zad1.c
@@ -0,0 +1,32 @@
+//
+// Created by gcyganek on 22.03.2021.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main(int argc, char** argv) {
+    if(argc != 2) {
+        fprintf(stderr, "Wrong number of arguments\n");
+        return 1;
+    }
+
+    pid_t child_pid;
+    long child_process_num = strtol(argv[1], NULL, 10);
+
+    while(child_process_num--) {
+        child_pid = fork();
+        if(child_pid == 0) {
+            printf("Message from child process pid:%d with parent process pid:%d\n", (int)getpid(), (int)getppid());
+            exit(0);
+        }
+    }
+
+    while (waitpid(-1, NULL, 0) != -1) {
+    }
+
+    return 0;
+}
